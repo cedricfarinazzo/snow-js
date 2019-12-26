55fc1a3f047665aad207dad06139f2c326d0d7e7
FARINAZZO Cédric <cedric.farinazzo@epita.fr>
2019-12-26 20:08:12 +0100

    add snow.js

---
+++ snow.js
@@ -0,0 +1,96 @@
+let Flake = class {
+    color = "#B0E5FF";
+    char = `❄️`;
+
+    wind = 0;
+
+    minXrange = -2;
+    maxXrange = 2;
+
+    minSpeed = 1.5;
+    maxSpeed = 4;
+
+    minSize = 18;
+    maxSize = 40;
+
+    WIDTH = 0;
+    HEIGHT = 0;
+    x = 0;
+    y = 0;
+    xrange = 0;
+    yspeed = 0;
+    life = 0;
+    size = 0;
+    html = null;
+
+    constructor() {
+        this.WIDTH = $(document).width();
+        this.HEIGHT = $(document).height();
+        this.spawn();
+        console.log(this);
+
+        this.html = document.createElement("span");
+        $(this.html).css('position', 'absolute');
+        $(this.html).css('fontSize', this.size + 'px');
+        $(this.html).css('color', this.color);
+        $(this.html).text(this.char);
+    }
+
+    spawn = () => {
+        this.life = 0;
+
+        this.xrange = Flake.random(this.minXrange, this.maxXrange);
+        this.yspeed = Flake.random(this.minSpeed, this.maxSpeed);
+        this.size = Flake.random(this.minSize, this.maxSize);
+
+        this.x = Flake.random(0, this.WIDTH - this.size - 5);
+        this.y = -this.maxSize;
+    };
+
+    render = () => {
+        $(this.html).css('top', this.y + 'px');
+        $(this.html).css('left', this.x + 'px');
+    };
+
+    animate = () => {
+        this.y += this.yspeed;
+        this.x += Math.sin(this.life) * this.xrange + this.wind;
+        this.life += .005;
+        if (this.x < this.maxSize || this.x >= this.WIDTH - this.maxSize - (this.size / 2)
+            || this.y >= this.HEIGHT - this.maxSize - this.size) {
+            this.spawn();
+        }
+        this.render();
+    };
+
+    get_html = () => {
+        return this.html;
+    };
+
+    static random(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+};
+
+let Snow = class {
+    flakes = [];
+
+    constructor(nb) {
+        this.frag = document.createDocumentFragment();
+
+        for (let i = 0; i < nb; i++) {
+            let flake = new Flake();
+            this.frag.appendChild(flake.get_html());
+            this.flakes.push(flake);
+        }
+
+        document.body.appendChild(this.frag);
+    }
+
+    animate = () => {
+        for (let i = 0; i < this.flakes.length; i++) {
+            this.flakes[i].animate();
+        }
+        setTimeout(this.animate, 20);
+    };
+};
